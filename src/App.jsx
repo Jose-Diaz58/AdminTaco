6274fae1b8723c1ebf443e2cefce876c2afd4f3a
Jose Diaz <joseluisdiaztrinidad2@gmail.com>
2026-03-03 15:58:50 -0600

    Rutas de la App

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import { useState } from 'react'
-import Formularios from './components/Formularios';
+import { BrowserRouter } from "react-router"
+import Routes from "./routes/Rutas"
 
-const Mensaje=()=>{
-  return<h2>Precio del taco: $15 c/U</h2>;
-};
 function App() {
-  const [contador, setcontador] = useState(0);
-  const Preciotaco = 15
-
-const Vendertaco=()=>setcontador(contador+1);
-const QuitarTaco=()=>setcontador(contador-1)
-
-  return (
-    <>
+  return(
     <div>
-      <Mensaje/>
+      <BrowserRouter>
+      <Routes/>
+      </BrowserRouter>
     </div>
-    <h3>Tacos pedidos: {contador}</h3>
-    <h3>Total a pagar: {contador*Preciotaco}</h3>
-    <h3>
-      <button onClick={Vendertaco}>+</button>
-      <button onClick={QuitarTaco}>-</button>
-    </h3>
-    <div>
-      <Formularios/>
-    </div>
-    </>
-    
   )
 }
 
---
+++ src/routes/Rutas.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Routes,Route} from "react-router";
+import {Home, Manager} from "../pages";
+import {ClienteLayout, AdminLayout} from "../layouts";
+
+export function Rutas() {
+  const LoadedLayout=(Layout, Page)=> {
+    return(
+      <Layout>
+        <Page/>
+      </Layout>
+    )
+  }
+  return (
+    <Routes>
+      <Route path= '' element={LoadedLayout(ClienteLayout,Home)}/>
+      <Route path= '/Manager' element={LoadedLayout(AdminLayout,Manager)}/>
+    </Routes>
+  )
+}
+export default Rutas
