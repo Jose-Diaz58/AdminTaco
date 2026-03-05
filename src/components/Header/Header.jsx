791c3d0205bf9a27b4dd2e392a1cbf85aa128b34
Chepe <hjosemanuel169@gmail.com>
2026-03-04 23:06:11 -0600

    Estoy bien menso no tenia el tailwind en el vite xd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export function Header() {
   return (
-   <header>texto corto de pubeva aver que tanto se uede escribit esta bien pero no se por que se corta malatido header ahora si aarra el puto</header>
+    <header className='flex items-center justify-between px-6 py-3 bg-white border-b shadow-sm'>
+        <div className='flex items-center gap-2 text-orange-500 font-bold text-xl'>🌮<span>Taqueria el amigo ahbram</span></div>
+        <button className='text-2xl text-gray-700 hover:text-orange-500'> ☰</button>
+    </header>
   );
 }
--- src/layouts/client/ClienteLayout.jsx
+++ src/layouts/client/ClienteLayout.jsx
@@ -2,9 +2,9 @@ import { Header } from "../../components";
 
 export function ClienteLayout({ children }) {
   return (
-   <div className="min-h-screen w-full bg-gray-50 flex flex-col">
+   <div className="min-h-screen bg-gray-100">
   <Header />
-  <main className="flex-1 p-4 md:p-8">
+  <main className="p-6">
     {children}
   </main>
 </div>
