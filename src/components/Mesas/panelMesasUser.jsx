bf7ba8dedcd87ba6d7a4fa621596bcec283c7018
Jose Diaz <joseluisdiaztrinidad2@gmail.com>
2026-05-28 12:46:50 -0600

    Cambios

--- src/components/Mesas/panelMesasUser.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function panelMesasUser() {
-  return (
-    <div>panelMesasUser</div>
-  )
-}
--- src/components/menu/MenuAdmin.jsx
+++ src/components/menu/MenuAdmin.jsx
@@ -51,7 +51,7 @@ export function MenuAdmin({ isOpen, toggleSidebar }) {
             className="flex items-center justify-center gap-2 bg-red-100 text-red-600 hover:bg-red-200 px-4 py-2.5 rounded-lg font-medium transition-colors w-full"
           >
             <LogOut size={18} />
-            <span>Cerrar Sesión</span>
+            <span>Pagina principal</span>
           </NavLink>
         </div>
 
